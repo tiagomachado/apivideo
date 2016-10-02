9d851d04d9059eb3477005b34233abe905f7e3f7
tiagomachado <tmachadoacm@gmail.com>
2016-10-02 19:42:44 +0100

    Add docblock

--- app/Http/Controllers/Api/V1/VideoController.php
+++ app/Http/Controllers/Api/V1/VideoController.php
@@ -8,11 +8,18 @@ use getID3;
 
 class VideoController extends Controller
 {
+    /**
+     * Save the uploaded video locally with random name
+     * and return getId3 in json format
+     * @param Request $video
+     * @param getID3 $getID3
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function postVideo(Request $video, getID3 $getID3)
     {
         $this->validate($video, ['video' => 'required']);
         $fileInfo = $getID3->analyze(uploadVideo($video));
 
-        return convertToUtf8($fileInfo);
+        return response()->json(convertToUtf8($fileInfo));
     }
 }
